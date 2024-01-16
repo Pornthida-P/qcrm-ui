fc411e6dc5bd43d30ce57bc95e66151b3f649c65
MrToNGx0 <tongsukkha@gmail.com>
2024-01-16 08:46:00 +0700

    Set Up #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/pages/home/home.component';
+import { NavbarComponent } from './features/components/navbar/navbar.component';
+import { FooterComponent } from './features/components/footer/footer.component';
+import { UserComponent } from './features/modals/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
