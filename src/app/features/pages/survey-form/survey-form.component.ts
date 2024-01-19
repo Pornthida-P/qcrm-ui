074735eb6c501cb777ec280139d0a0225cf7f270
Benjapronben <121014441+Benjapronben@users.noreply.github.com>
2024-01-19 17:29:49 +0700

    เพิ่มหน้าแบบฟอร์มสำรวจ #11

--- src/app/features/pages/survey-form/survey-form.component.ts
+++ src/app/features/pages/survey-form/survey-form.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-survey-form',
   templateUrl: './survey-form.component.html',
   styleUrls: ['./survey-form.component.scss']
 })
-export class SurveyFormComponent {
+export class SurveyFormComponent implements OnInit{
+
+  surveyForms!: any[]
+  selectedSurveyForms: any | undefined;
+
+  value: string | undefined;
+
+  filterOption!: any[];
+  selectedFilter: any | undefined;
+  faPenToSquare = faPenToSquare;
+
+  ngOnInit() {
+
+    this.filterOption = [
+      { name: 'ทั้งหมด', code: 'all' },
+      { name: 'Only My', code: 'me' },
+    ];
+    this.selectedFilter = this.filterOption[0];
+
+    this.surveyForms = [
+        {
+            name: 'Admin',
+            survey_form:
+                'แบบสำรวจติดตามและประเมินผลเพื่อพัฒนาศักยภาพด้านการค้าระหว่างประเทศ "สถาบันพัฒนาผู้ประกอบการการค้ายุคใหม่(NEA) กรมส่งเสริมการค้าระหว่างประเทศ กระทรวงพาณิชย์” สำหรับประเภทผู้ประกอบการรุ่นใหม่ (นิสิต/นักศึกษา) ที่เข้าร่วมโครงการใน FromGen Z to be CEO 2023',
+            save_date: '25 ก.ย., 2023 10:24',
+        },
+        {
+            name: 'Admin',
+            survey_form:
+                'แบบสำรวจติดตามและประเมินผลเพื่อพัฒนาศักยภาพด้านการค้าระหว่างประเทศ "สถาบันพัฒนาผู้ประกอบการการค้ายุคใหม่(NEA) กรมส่งเสริมการค้าระหว่างประเทศ กระทรวงพาณิชย์” สำหรับประเภทผู้ประกอบการรุ่นใหม่ (นิสิต/นักศึกษา) ที่เข้าร่วมโครงการใน FromGen Z to be CEO 2023',
+            save_date: '25 ก.ย., 2023 10:30',
+        },
+        {
+            name: 'Admin',
+            survey_form:
+                'แบบสำรวจติดตามและประเมินผลเพื่อพัฒนาศักยภาพด้านการค้าระหว่างประเทศ "สถาบันพัฒนาผู้ประกอบการการค้ายุคใหม่(NEA) กรมส่งเสริมการค้าระหว่างประเทศ กระทรวงพาณิชย์” สำหรับประเภทผู้ประกอบการรุ่นใหม่ (นิสิต/นักศึกษา) ที่เข้าร่วมโครงการใน FromGen Z to be CEO 2023',
+            save_date: '25 ก.ย., 2023 10:40',
+        },
+    ];
+}
 
 }
--- src/app/features/pages/survey-form/survey-form.module.ts
+++ src/app/features/pages/survey-form/survey-form.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SurveyFormComponent } from './survey-form.component';
 import { RouterModule } from '@angular/router';
-
+import { TableModule } from 'primeng/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TooltipModule } from 'primeng/tooltip';
 @NgModule({
     declarations: [SurveyFormComponent],
     imports: [
-        CommonModule,
+      CommonModule,
+      TableModule,
+      FormsModule,
+      ReactiveFormsModule,
+      InputTextModule,
+      ButtonModule,
+      DropdownModule,
+      FontAwesomeModule,
+      TooltipModule,
         RouterModule.forChild([
             {
                 path: '',
