c1382cfd1d0c3849ea8d72c2f483d802accd99a7
Benjapronben <121014441+Benjapronben@users.noreply.github.com>
2024-01-22 10:06:49 +0700

    add manage-survey-form #11

---
+++ src/app/features/pages/manage-survey-form/manage-survey-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-survey-form',
+  templateUrl: './manage-survey-form.component.html',
+  styleUrls: ['./manage-survey-form.component.scss']
+})
+export class ManageSurveyFormComponent {
+
+}
