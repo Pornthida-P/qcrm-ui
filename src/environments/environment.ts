3b96cf66abc8f3829a0a4973d035b814131d9f92
MrToNGx0 <tongsukkha@gmail.com>
2024-01-23 15:42:31 +0700

    add endpoint api page contact #22

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,13 @@ export const environment = {
     production: true,
     api: {
         url: 'http://localhost:3000',
+        endpoint: {
+            contact: {
+                findAll: '/contact/all',
+                findById: '/contact/id',
+                findByPage: '/contact/pages',
+                countAllItem: '/contact/countAllItem',
+            },
+        },
     },
 };
