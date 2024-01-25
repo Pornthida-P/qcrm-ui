6035b88c0086520a0694793b79470f99cec5f972
Benjapronben <121014441+Benjapronben@users.noreply.github.com>
2024-01-25 15:37:53 +0700

    Add button back manage-survey-form #11

--- src/app/features/pages/manage-survey-form/manage-survey-form.component.ts
+++ src/app/features/pages/manage-survey-form/manage-survey-form.component.ts
@@ -1,13 +1,22 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-
+import {Location} from '@angular/common';
 @Component({
     selector: 'app-manage-survey-form',
     templateUrl: './manage-survey-form.component.html',
     styleUrls: ['./manage-survey-form.component.scss'],
 })
 export class ManageSurveyFormComponent {
+
+    constructor(private _location: Location)
+    {}
+
     form: any = {};
     onChange(event: any) {
         console.log(event.component);
     }
+
+    prev() {
+      this._location.back();
+    }
+
 }
