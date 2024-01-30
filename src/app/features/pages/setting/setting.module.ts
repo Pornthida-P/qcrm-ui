e0f97b277a9b8791fdd1c4658cf6fa1a2534f050
MrToNGx0 <tongsukkha@gmail.com>
2024-01-30 09:17:44 +0700

    add page setting #39

--- src/app/features/pages/setting/setting.module.ts
+++ src/app/features/pages/setting/setting.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SettingComponent } from './setting.component';
-import { RouterModule } from '@angular/router';
+import { SettingRoutingModule } from './setting-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [SettingComponent],
-    imports: [
-        CommonModule,
-        RouterModule.forChild([
-            {
-                path: '',
-                component: SettingComponent,
-            },
-        ]),
-    ],
+    imports: [CommonModule, FontAwesomeModule, SettingRoutingModule, FormsModule, ReactiveFormsModule],
 })
 export class SettingModule {}
--- src/app/features/pages/survey-form/survey-form.module.ts
+++ src/app/features/pages/survey-form/survey-form.module.ts
@@ -7,7 +7,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import { SurveyFormService } from 'src/app/services/survey-form/survey-form.service';
 import { NgbdSortableHeader } from './sortable.directive';
-import * as bootstrap from 'bootstrap';
+
 @NgModule({
     declarations: [SurveyFormComponent],
     imports: [
@@ -22,7 +22,7 @@ import * as bootstrap from 'bootstrap';
             },
         ]),
         NgbPaginationModule,
-      NgbdSortableHeader,
+        NgbdSortableHeader,
     ],
     providers: [SurveyFormService, DecimalPipe],
 })
