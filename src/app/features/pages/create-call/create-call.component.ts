7c758b1ac04278bdf912d3c5941ebb3e98fd99d0
Benjapronben <121014441+Benjapronben@users.noreply.github.com>
2024-01-31 16:37:33 +0700

    add create call page #54

---
+++ src/app/features/pages/create-call/create-call.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-call',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-call.component.html',
+  styleUrl: './create-call.component.scss'
+})
+export class CreateCallComponent {
+
+}
