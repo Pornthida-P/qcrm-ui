d1d52dea49085d5b7bf8902f0a6ced71bf92204e
MrToNGx0 <tongsukkha@gmail.com>
2024-02-01 16:56:07 +0700

    add component profile list #44

---
+++ src/app/features/components/profile-list/profile-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { User } from 'src/app/shared/interface/user.interface';
+
+@Component({
+    selector: 'app-profile-list',
+    templateUrl: './profile-list.component.html',
+    styleUrl: './profile-list.component.scss',
+})
+export class ProfileListComponent {
+    @Input() member?: User;
+
+    profileError: string = '/assets/nea-qcrm-ui/image/profile/user.jpg';
+
+    handleProfileError(event: any) {
+        if (event) {
+            event.target.src = this.profileError;
+        }
+    }
+}
