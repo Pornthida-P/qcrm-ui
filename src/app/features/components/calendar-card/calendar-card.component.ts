ebac51eea2b883efb75ee63436cdacf7bb20768c
MrToNGx0 <tongsukkha@gmail.com>
2024-02-02 15:48:48 +0700

    adjust class calendar card #44

--- src/app/features/components/calendar-card/calendar-card.component.ts
+++ src/app/features/components/calendar-card/calendar-card.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { faCalendarAlt, faList, faLocationDot, faUserGroup, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { catchError, tap } from 'rxjs';
+import { CalendarEventService } from 'src/app/services/calendar-event/calendar-event.service';
 import { ModalCalendarService } from 'src/app/services/modal-calendar/modal-calendar.service';
+import { SweetAlertService } from 'src/app/services/sweet-alert/sweet-alert.service';
 import { CalendarEvent } from 'src/app/shared/interface/calendar.interface';
 
 @Component({
@@ -20,7 +23,11 @@ export class CalendarCardComponent implements OnInit {
     faEdit = faEdit;
     faTrash = faTrash;
 
-    constructor(private modalCalendarService: ModalCalendarService) {}
+    constructor(
+        private modalCalendarService: ModalCalendarService,
+        private calendarService: CalendarEventService,
+        private sweetalertServices: SweetAlertService,
+    ) {}
 
     ngOnInit(): void {}
 
@@ -31,12 +38,46 @@ export class CalendarCardComponent implements OnInit {
     }
 
     onClickEditEvent(event: CalendarEvent | undefined) {
-        if (event) {
+        if (event && event?.eventId) {
             this.modalCalendarService.openDialog('edit', event);
         }
     }
 
-    onClickDeleteEvent() {
-        console.log(`Delete Event`);
+    onClickDeleteEvent(event: CalendarEvent | undefined) {
+        if (event && event?.eventId) {
+            this.calendarService
+                .deleteCalendarEvent(event.eventId.toString())
+                .pipe(
+                    catchError((error) => {
+                        this.handleContactError(error);
+                        throw error;
+                    }),
+                )
+                .subscribe(() => {});
+        }
+    }
+
+    handleContactError(error: any) {
+        let icon: string;
+        let errorMessage: string;
+        let title: string;
+        let route: string;
+
+        switch (error.status) {
+            case 401:
+                icon = 'warning';
+                title = 'Warning Authentication';
+                errorMessage = 'Your session has expired. Please log in again.';
+                route = 'login';
+                break;
+            default:
+                icon = 'error';
+                title = 'Calendar Event Error';
+                errorMessage = 'Failed to update calendar event. Please try again later.';
+                route = '';
+                break;
+        }
+
+        this.sweetalertServices.getSwal(icon, title, errorMessage, false, route);
     }
 }
