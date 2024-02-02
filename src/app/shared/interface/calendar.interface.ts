b2e4fdad2f1c42c748040c399850716ada5d2de9
MrToNGx0 <tongsukkha@gmail.com>
2024-02-02 15:51:13 +0700

    add eventId in interface #44

--- src/app/shared/interface/calendar.interface.ts
+++ src/app/shared/interface/calendar.interface.ts
@@ -1,8 +1,9 @@
 import { User } from './user.interface';
 
 export interface CalendarEvent {
+    eventId: number;
     title: string;
-    location: string;
+    location?: string;
     datetime: string;
     description: string;
     members: User[];
