4ea59c0640712bd4daba7a4d9fab4b70e709b9c1
MrToNGx0 <tongsukkha@gmail.com>
2024-02-02 15:48:19 +0700

    add path endpoint calendar #44

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,6 +2,20 @@ export const config = {
     api: {
         path: {
             login: '/login',
+            user: {
+                findAll: '/user',
+                findById: '/user/:id',
+                findByRole: '/user/role/:role',
+            },
+            calendarEvent: {
+                findAll: '/calendar-event',
+                findById: '/calendar-event/:id',
+                findByDate: '/calendar-event/date/:date',
+                findByMember: '/calendar-event/member/:id',
+                add: '/calendar-event/add',
+                update: '/calendar-event/update',
+                delete: '/calendar-event/delete',
+            },
             elearning: '/e-learning',
             surveyForm: {
                 baseUrl: '/survey-form',
