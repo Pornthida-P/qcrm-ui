c727640cf51b85f845589164b60d95526d53b27f
Benjapronben <121014441+Benjapronben@users.noreply.github.com>
2024-02-06 17:33:59 +0700

    add change call page #63

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Formio, FormioModule } from '@formio/angular';
 import bootstrap4 from '@formio/bootstrap/bootstrap4';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule,NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { TokenInterceptor } from './core/interceptor/token.interceptor';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+
 (Formio as any).use(bootstrap4);
 
 @NgModule({
@@ -27,7 +28,8 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
         FormioModule,
         NgbModule,
         MatDatepickerModule,
-        MatNativeDateModule,
+      MatNativeDateModule,
+      NgbDatepickerModule,
     ],
     providers: [
         {
