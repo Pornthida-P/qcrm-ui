957506fcef4869eb3b1d9c47cd8e152417e55fa0
MrToNGx0 <tongsukkha@gmail.com>
2024-02-07 17:07:24 +0700

    add alert invalid value required #44

--- src/app/features/modals/menagement-calendar/menagement-calendar.component.ts
+++ src/app/features/modals/menagement-calendar/menagement-calendar.component.ts
@@ -1,14 +1,16 @@
-import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { faCalendarAlt, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarAlt, faPaperclip, faPlusCircle, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
 import * as moment from 'moment';
 import { catchError, tap } from 'rxjs';
+import { AttachmentService } from 'src/app/services/attachment/attachment.service';
 import { CalendarEventService } from 'src/app/services/calendar-event/calendar-event.service';
 import { SweetAlertService } from 'src/app/services/sweet-alert/sweet-alert.service';
 import { UserService } from 'src/app/services/user/user.service';
-import { CalendarEvent } from 'src/app/shared/interface/calendar.interface';
+import { Attachment } from 'src/app/shared/interface/attachment.interface';
+import { CalendarEvent, CalendarTag } from 'src/app/shared/interface/calendar.interface';
 import { User } from 'src/app/shared/interface/user.interface';
 
 @Component({
@@ -22,11 +24,17 @@ export class MenagementCalendarComponent implements OnInit {
     members: User[] = [];
     userData: User | null = null;
     selectedMembers: User[] = [];
+    attachments: Attachment[] = [];
+    tags: CalendarTag[] = [];
+
+    @ViewChild('fileInput') fileInput: ElementRef | undefined;
 
     profileError: string = './assets/nea-qcrm-ui/image/profile/user.jpg';
 
     faXmark = faXmark;
     faCalendar = faCalendarAlt;
+    faAttachment = faPaperclip;
+    faPlus = faPlusCircle;
 
     editorConfig: AngularEditorConfig = {
         editable: true,
@@ -68,8 +76,6 @@ export class MenagementCalendarComponent implements OnInit {
         toolbarPosition: 'top',
         toolbarHiddenButtons: [
             ['font'],
-            ['insertImage'],
-            ['insertVideo'],
             ['insertHorizontalRule'],
             ['removeFormat'],
             ['toggleEditor'],
@@ -82,10 +88,11 @@ export class MenagementCalendarComponent implements OnInit {
         private fb: FormBuilder,
         private userService: UserService,
         private calendarService: CalendarEventService,
+        private attachmentService: AttachmentService,
         private sweetalertServices: SweetAlertService,
         private cdRef: ChangeDetectorRef,
         public dialogRef: MatDialogRef<MenagementCalendarComponent>,
-        @Inject(MAT_DIALOG_DATA) public data: { mode: 'add' | 'edit'; eventData?: CalendarEvent },
+        @Inject(MAT_DIALOG_DATA) public data: { mode: 'add' | 'view' | 'edit'; eventData?: CalendarEvent },
     ) {}
 
     ngOnInit(): void {
@@ -96,10 +103,10 @@ export class MenagementCalendarComponent implements OnInit {
 
     initializeForm(): void {
         if (this.data.mode === 'edit' && this.data.eventData) {
-            console.log(this.data.eventData);
             this.calendarEvent = this.fb.group({
                 eventId: [this.data.eventData.eventId],
                 title: [this.data.eventData.title, Validators.required],
+                tag: [this.data.eventData.tag.tagId, Validators.required],
                 location: [this.data.eventData.location],
                 startDate: [new Date(this.data.eventData.startDate).toISOString(), Validators.required],
                 endDate: [new Date(this.data.eventData.startDate).toISOString(), Validators.required],
@@ -110,6 +117,7 @@ export class MenagementCalendarComponent implements OnInit {
             this.calendarEvent = this.fb.group({
                 eventId: [''],
                 title: ['', Validators.required],
+                tag: ['', Validators.required],
                 location: [''],
                 startDate: [new Date().toISOString(), Validators.required],
                 endDate: [new Date().toISOString(), Validators.required],
@@ -117,12 +125,30 @@ export class MenagementCalendarComponent implements OnInit {
                 members: [[], Validators.required],
             });
         }
+        this.attachments = [...(this.data.eventData?.attachments || [])];
+
+        this.getAllTags();
     }
 
     getUserData() {
         this.userService.getDataUser().subscribe((user: User | null) => {
             this.userData = user;
         });
+    }
+
+    getAllTags() {
+        this.calendarService
+            .findAllTags()
+            .pipe(
+                tap((tags) => {
+                    this.tags = tags;
+                }),
+                catchError((error) => {
+                    this.handleContactError(error);
+                    throw error;
+                }),
+            )
+            .subscribe();
     }
 
     async getMembers() {
@@ -157,13 +183,24 @@ export class MenagementCalendarComponent implements OnInit {
 
     onSubmit() {
         if (this.calendarEvent.invalid) {
+            this.sweetalertServices.getSwal('warning', 'Warning', 'Please fill in all required fields.', false, '');
             return;
         }
 
         const formData = this.calendarEvent.value;
-        formData.datetime = moment(formData.datetime).format('YYYY-MM-DD HH:mm:ss');
+        const startDateTime = moment(formData.startDate);
+        const endDateTime = moment(formData.endDate);
+
+        if (!startDateTime.isBefore(endDateTime)) {
+            this.sweetalertServices.getSwal('warning', 'Warning', 'Start date must be before end date.', false, '');
+            return;
+        }
+
+        formData.startDate = moment(formData.startDate).startOf('day').format('YYYY-MM-DD HH:mm:ss');
+        formData.endDate = moment(formData.endDate).endOf('day').format('YYYY-MM-DD HH:mm:ss');
         formData.createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
         formData.createdById = this.userData?.userId;
+        formData.attachments = this.attachments;
 
         if (this.data.mode === 'add') {
             this.calendarService
@@ -178,6 +215,11 @@ export class MenagementCalendarComponent implements OnInit {
         }
 
         if (this.data.mode === 'edit') {
+            if (!formData.eventId) {
+                this.sweetalertServices.getSwal('warning', 'Warning', 'Invalid data. Please check your input and try again.', false, '');
+                return;
+            }
+
             this.calendarService
                 .updateCalendarEvent(formData.eventId, formData)
                 .pipe(
@@ -194,10 +236,39 @@ export class MenagementCalendarComponent implements OnInit {
         this.dialogRef.close();
     }
 
+    onFileSelected(event: any) {
+        const file = event.target.files[0];
+        const createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+        this.attachmentService
+            .upload(file, file.name, createdAt, this.userData?.userId || '')
+            .pipe(
+                tap((response: any) => {
+                    console.log('Attachment response:', response);
+                    this.attachments.push(response);
+                }),
+                catchError((error) => {
+                    this.handleContactError(error);
+                    throw error;
+                }),
+            )
+            .subscribe(() => {});
+    }
+
+    openFileInput() {
+        if (this.fileInput) {
+            this.fileInput.nativeElement.click();
+        }
+    }
+
     handleProfileError(event: any) {
         if (event) {
             event.target.src = this.profileError;
         }
+    }
+
+    onDeletedMember(userId: string) {
+        this.selectedMembers = this.selectedMembers.filter((member) => member.userId !== userId);
+        this.calendarEvent.get('members')!.setValue(this.selectedMembers);
     }
 
     handleContactError(error: any) {
