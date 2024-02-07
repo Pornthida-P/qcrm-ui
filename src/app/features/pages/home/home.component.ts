93ed010a3663aef9a32927dd3f98beb08aee84f1
MrToNGx0 <tongsukkha@gmail.com>
2024-02-07 17:09:09 +0700

    declaration and import module #44

--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { faCalendar, faList } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+    selectedViewType = 'calendar';
+
+    faCalendar = faCalendar;
+    faList = faList;
+
+    constructor() {}
+}
