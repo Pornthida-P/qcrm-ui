a4f07b5f99c451f8f864ad28834de0cf6aa61b30
MrToNGx0 <tongsukkha@gmail.com>
2024-02-07 17:03:09 +0700

    add attachment component #44

---
+++ src/app/features/components/attachments/attachments.component.ts
@@ -0,0 +1,121 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+    IconDefinition,
+    faDownload,
+    faEye,
+    faFile,
+    faFileExcel,
+    faFileImage,
+    faFilePdf,
+    faFileText,
+    faFileWord,
+    faXmark,
+} from '@fortawesome/free-solid-svg-icons';
+import { catchError } from 'rxjs';
+import { AttachmentService } from 'src/app/services/attachment/attachment.service';
+import { SweetAlertService } from 'src/app/services/sweet-alert/sweet-alert.service';
+import { Attachment } from 'src/app/shared/interface/attachment.interface';
+
+@Component({
+    selector: 'app-attachments',
+    templateUrl: './attachments.component.html',
+    styleUrl: './attachments.component.scss',
+})
+export class AttachmentsComponent implements OnInit {
+    @Input() attachment?: Attachment;
+    @Input() isAction: boolean = false;
+    @Output() deleteAttachmentId: EventEmitter<string> = new EventEmitter<string>();
+
+    faFileImage = faFileImage;
+    faFileDdf = faFilePdf;
+    faFileWord = faFileWord;
+    faFileExcel = faFileExcel;
+    faFileText = faFileText;
+    faFile = faFile;
+    faEye = faEye;
+    faDownload = faDownload;
+    faXmark = faXmark;
+
+    constructor(private attachmentService: AttachmentService, private sweetalertServices: SweetAlertService) {}
+
+    ngOnInit(): void {}
+
+    getIconClass(fileType: string): IconDefinition {
+        switch (fileType) {
+            case 'image/png':
+            case 'image/jpeg':
+            case 'image/gif':
+                return faFileImage;
+            case 'application/pdf':
+                return faFilePdf;
+            case 'application/msword':
+            case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                return faFileWord;
+            case 'application/vnd.ms-excel':
+            case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+                return faFileExcel;
+            case 'text/plain':
+                return faFileText;
+            default:
+                return faFile;
+        }
+    }
+
+    onClickViewAttachment(attachment: Attachment) {
+        if (attachment.filepath) {
+            this.attachmentService.view(attachment.filepath);
+        } else {
+            this.sweetalertServices.getSwal('warning', 'Warning', 'There is a problem with the internet. Please try again.', false, '');
+        }
+    }
+
+    onClickDownloadAttachment(attachment: Attachment) {
+        if (attachment.filepath) {
+            this.attachmentService.download(attachment.filepath);
+        } else {
+            this.sweetalertServices.getSwal('warning', 'Warning', 'There is a problem with the internet. Please try again.', false, '');
+        }
+    }
+
+    onClickDeleteAttachment(attachment: Attachment) {
+        if (attachment.attachmentId) {
+            this.attachmentService
+                .delete(attachment.attachmentId)
+                .pipe(
+                    catchError((error) => {
+                        this.handleCalendarEventError(error);
+                        throw error;
+                    }),
+                )
+                .subscribe(() => {
+                    this.deleteAttachmentId.emit(attachment.attachmentId);
+                });
+        } else {
+            this.sweetalertServices.getSwal('warning', 'Warning', 'There is a problem with the internet. Please try again.', false, '');
+        }
+    }
+
+    handleCalendarEventError(error: any) {
+        let icon: string;
+        let errorMessage: string;
+        let title: string;
+        let route: string;
+
+        switch (error.status) {
+            case 401:
+                icon = 'warning';
+                title = 'Warning Authentication';
+                errorMessage = 'Your session has expired. Please log in again.';
+                route = 'login';
+                break;
+            default:
+                icon = 'error';
+                title = 'Calendar Event Error';
+                errorMessage = 'Failed to update calendar event. Please try again later.';
+                route = '';
+                break;
+        }
+
+        this.sweetalertServices.getSwal(icon, title, errorMessage, false, route);
+    }
+}
