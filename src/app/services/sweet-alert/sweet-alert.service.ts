544fcdd4f4e557ca0fcb8311e87753ffd4e43b70
MrToNGx0 <tongsukkha@gmail.com>
2024-02-12 13:21:53 +0700

    add function handle Error #72

--- src/app/services/sweet-alert/sweet-alert.service.ts
+++ src/app/services/sweet-alert/sweet-alert.service.ts
@@ -15,10 +15,44 @@ export class SweetAlertService {
             text: text,
             showConfirmButton: showButton,
             confirmButtonColor: '#0a6ebd',
-        }).then((result) => {
+        }).then(() => {
             if (route) {
                 setTimeout(() => this.router.navigate([`/${route}`]), 500);
             }
         });
+    }
+
+    handleError(error: any) {
+        let icon: string;
+        let errorMessage: string;
+        let title: string;
+        let route: string = '';
+
+        switch (error.status) {
+            case 0:
+                icon = 'error';
+                title = 'Connection Error';
+                errorMessage = 'Failed to connect to the server. Please check your internet connection and try again.';
+                break;
+            case 400:
+                icon = 'error';
+                title = 'Error';
+                errorMessage = error.error.message || 'An error occurred.';
+                break;
+            case 401:
+                icon = 'warning';
+                title = 'Warning Authentication';
+                errorMessage = 'Your session has expired. Please log in again.';
+                route = 'login';
+                break;
+            default:
+                icon = 'error';
+                title = 'Error Network';
+                errorMessage = 'Failed to request. Please try again later.';
+                route = '';
+                break;
+        }
+
+        this.getSwal(icon, title, errorMessage, false, route);
     }
 }
