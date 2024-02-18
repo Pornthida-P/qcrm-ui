3f3debb0f39d3c6105e0890afc3fb7a9e91998e5
MrToNGx0 <tongsukkha@gmail.com>
2024-02-18 14:33:17 +0700

    adjust component profile and profile list #80

--- src/app/features/components/profile/profile.component.ts
+++ src/app/features/components/profile/profile.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { faEye, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faEye, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { ModalUserService } from 'src/app/services/modal-user/modal-user.service';
 import { SweetAlertService } from 'src/app/services/sweet-alert/sweet-alert.service';
+import { UserService } from 'src/app/services/user/user.service';
 import { User } from 'src/app/shared/interface/user.interface';
 
 @Component({
@@ -13,16 +14,26 @@ export class ProfileComponent implements OnInit {
     @Input() member?: User;
     @Input() mode?: 'view' | 'edit' | 'add';
     @Input() isShowToolbar?: boolean = false;
+    @Input() isBackground?: boolean = true;
     @Output() deleteUserId: EventEmitter<string> = new EventEmitter<string>();
 
+    userData?: User | null;
+    isAction: boolean = false;
     profileError: string = './assets/nea-qcrm-ui/image/profile/user.jpg';
 
     faEye = faEye;
+    faEdit = faEdit;
     faXmark = faXmark;
 
-    constructor(private sweetAlertService: SweetAlertService, private modalUserService: ModalUserService) {}
+    constructor(
+        private sweetAlertService: SweetAlertService,
+        private modalUserService: ModalUserService,
+        private userService: UserService,
+    ) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.getUserData();
+    }
 
     handleProfileError(event: any) {
         if (event) {
@@ -30,11 +41,22 @@ export class ProfileComponent implements OnInit {
         }
     }
 
-    onClickViewProfile(member: User | undefined) {
+    getUserData() {
+        this.userService.getDataUser().subscribe((res: User | null) => {
+            this.userData = res;
+            this.isAction = res?.role.roleTitle.toLowerCase() === 'admin' ? true : false;
+        });
+    }
+
+    onClickView(member: User | undefined) {
         this.modalUserService.openDialog('view', member);
     }
 
-    onClickDeleteProfile(member: User | undefined) {
+    onClickEdit(member: User | undefined) {
+        this.modalUserService.openDialog('edit', member);
+    }
+
+    onClickDelete(member: User | undefined) {
         if (member?.userId) {
             this.deleteUserId.emit(member.userId);
         } else {
