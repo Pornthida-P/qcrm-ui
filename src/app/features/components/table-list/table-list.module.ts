3c050b16b99beebce88112d6875cf44e8df3022a
MrToNGx0 <tongsukkha@gmail.com>
2024-02-22 09:03:41 +0700

    add module #80

--- src/app/features/components/table-list/table-list.module.ts
+++ src/app/features/components/table-list/table-list.module.ts
@@ -6,10 +6,20 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
     declarations: [TableListComponent],
-    imports: [CommonModule, FontAwesomeModule, MatTableModule, FormsModule, ReactiveFormsModule, MatSelectModule, MatMenuModule],
+    imports: [
+        CommonModule,
+        FontAwesomeModule,
+        MatTableModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatSelectModule,
+        MatMenuModule,
+        MatPaginatorModule,
+    ],
     exports: [TableListComponent],
 })
 export class TableListModule {}
