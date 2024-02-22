c3c138b41d75afbd0554013560e8f93468be7a4e
MrToNGx0 <tongsukkha@gmail.com>
2024-02-22 15:09:44 +0700

    update code #80

--- src/app/features/modals/menagement-tag/menagement-tag.component.ts
+++ src/app/features/modals/menagement-tag/menagement-tag.component.ts
@@ -2,7 +2,9 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import * as moment from 'moment';
 import { tap } from 'rxjs';
+import { CalendarEventService } from 'src/app/services/calendar-event/calendar-event.service';
 import { SweetAlertService } from 'src/app/services/sweet-alert/sweet-alert.service';
 import { UserService } from 'src/app/services/user/user.service';
 import { CalendarTag } from 'src/app/shared/interface/calendar.interface';
@@ -24,6 +26,7 @@ export class MenagementTagComponent implements OnInit {
         private fb: FormBuilder,
         private userService: UserService,
         private sweetalertServices: SweetAlertService,
+        private calendarService: CalendarEventService,
         public dialogRef: MatDialogRef<MenagementTagComponent>,
         @Inject(MAT_DIALOG_DATA) public data: { mode: 'add' | 'view' | 'edit'; tag?: CalendarTag },
     ) {}
@@ -57,6 +60,7 @@ export class MenagementTagComponent implements OnInit {
                 tagId: [{ value: '', disabled: isViewMode }],
                 tagName: [{ value: '', disabled: isViewMode }, [Validators.required]],
                 description: [{ value: '', disabled: isViewMode }, [Validators.required]],
+                color: [{ value: '', disabled: isViewMode }, [Validators.required]],
                 createdAt: [{ value: '', disabled: isViewMode }],
                 createdById: [{ value: '', disabled: isViewMode }],
                 modifyAt: [{ value: '', disabled: isViewMode }],
@@ -67,6 +71,7 @@ export class MenagementTagComponent implements OnInit {
                 tagId: [{ value: this.data.tag?.tagId, disabled: isViewMode }],
                 tagName: [{ value: this.data.tag?.tagName, disabled: isViewMode }, [Validators.required]],
                 description: [{ value: this.data.tag?.description, disabled: isViewMode }, [Validators.required]],
+                color: [{ value: this.data.tag?.color, disabled: isViewMode }, [Validators.required]],
                 createdAt: [{ value: this.data.tag?.createdAt, disabled: isViewMode }],
                 createdById: [{ value: this.data.tag?.createdById, disabled: isViewMode }],
                 modifyAt: [{ value: this.data.tag?.modifyAt, disabled: isViewMode }],
@@ -80,6 +85,44 @@ export class MenagementTagComponent implements OnInit {
     }
 
     onSubmit(): void {
-        console.log('Submit:', this.tagData.value);
+        if (this.tagData.invalid) {
+            this.sweetalertServices.getSwal('warning', 'warning', 'Please fill in the required fields.', false, '');
+            return;
+        }
+
+        const tagData: CalendarTag = {
+            tagId: this.tagData.get('tagId')?.value,
+            tagName: this.tagData.get('tagName')?.value,
+            description: this.tagData.get('description')?.value,
+            color: this.tagData.get('color')?.value,
+        };
+
+        if (this.data.mode === 'add') {
+            const createdAt = moment().format('YYYY-MM-DDTHH:mm:ss');
+            const createdById = this.userData?.userId;
+
+            this.calendarService
+                .addCalendarTag({ ...tagData, createdAt, createdById })
+                .pipe(
+                    tap(() => {
+                        this.sweetalertServices.getSwal('success', 'success', 'Tag added successfully', false, '');
+                        this.dialogRef.close(tagData);
+                    }),
+                )
+                .subscribe(() => {});
+        } else if (this.data.mode === 'edit') {
+            const modifyAt = moment().format('YYYY-MM-DDTHH:mm:ss');
+            const modifyById = this.userData?.userId;
+
+            this.calendarService
+                .updateCalendarTag({ ...tagData, modifyAt, modifyById })
+                .pipe(
+                    tap(() => {
+                        this.sweetalertServices.getSwal('success', 'success', 'Tag updated successfully', false, '');
+                        this.dialogRef.close(tagData);
+                    }),
+                )
+                .subscribe(() => {});
+        }
     }
 }
