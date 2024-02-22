61d70ec14f24291d4b5f669722b0d860a713e82b
MrToNGx0 <tongsukkha@gmail.com>
2024-02-22 09:05:27 +0700

    update code #80

--- src/app/features/components/tag/tag.component.ts
+++ src/app/features/components/tag/tag.component.ts
@@ -1,28 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { faGear, faEye, faEdit, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { catchError, tap } from 'rxjs';
 import { CalendarEventService } from 'src/app/services/calendar-event/calendar-event.service';
+import { ModalTagService } from 'src/app/services/modal-tag/modal-tag.service';
 import { SweetAlertService } from 'src/app/services/sweet-alert/sweet-alert.service';
 import { UserService } from 'src/app/services/user/user.service';
 import { CalendarTag } from 'src/app/shared/interface/calendar.interface';
+import { User } from 'src/app/shared/interface/user.interface';
 
 @Component({
     selector: 'app-tag',
     templateUrl: './tag.component.html',
     styleUrl: './tag.component.scss',
 })
-export class TagComponent implements OnInit {
+export class TagComponent implements OnInit, OnChanges {
     title = 'แท็ก';
-
     tags: CalendarTag[] = [];
+    displayedColumns: string[] = [];
+    dataSource = new MatTableDataSource<CalendarTag>(this.tags);
+    initialColumnVisibility: { [key: string]: boolean } = {
+        tagId: false,
+        tagName: false,
+        description: false,
+        createdAt: false,
+        createdById: false,
+        modifyAt: false,
+        modifyById: false,
+    };
+    columnVisibility: { [key: string]: boolean } = { ...this.initialColumnVisibility };
+    showColumnMenu = false;
+    isAction: boolean = false;
+    userDatas: User | null = null;
 
-    constructor(private calendarService: CalendarEventService, private sweetAlertService: SweetAlertService) {}
+    faGear = faGear;
+    faEye = faEye;
+    faEdit = faEdit;
+    faXmark = faXmark;
+
+    get columnVisibilityKeys(): string[] {
+        return Object.keys(this.columnVisibility);
+    }
+
+    constructor(
+        private calendarService: CalendarEventService,
+        private sweetAlertService: SweetAlertService,
+        private userService: UserService,
+        private modalTagService: ModalTagService,
+    ) {}
 
     ngOnInit(): void {
         this.initzation();
     }
 
+    ngOnChanges(): void {
+        this.initzation();
+    }
+
     initzation(): void {
         this.findAllTag();
+        this.getUerData();
     }
 
     findAllTag(): void {
@@ -31,6 +68,15 @@ export class TagComponent implements OnInit {
             .pipe(
                 tap((tags) => {
                     this.tags = tags;
+                    if (this.tags && this.tags.length > 0) {
+                        this.displayedColumns = ['tagName', 'description', 'createdAt'];
+                        this.dataSource = new MatTableDataSource<CalendarTag>(this.tags);
+
+                        this.displayedColumns.forEach((column) => (this.columnVisibility[column] = true));
+                    } else {
+                        this.displayedColumns = [];
+                        this.dataSource.data = [];
+                    }
                 }),
                 catchError((error) => {
                     this.sweetAlertService.handleError(error);
@@ -40,7 +86,55 @@ export class TagComponent implements OnInit {
             .subscribe(() => {});
     }
 
+    getUerData(): void {
+        this.userService.getDataUser().subscribe((user) => {
+            this.userDatas = user;
+            this.isAction = this.userDatas?.role?.roleTitle.toLowerCase() === 'admin' ? true : false;
+        });
+    }
+
+    setDisplayFields(fields: string[]): void {
+        this.displayedColumns = fields;
+        const allColumns = Object.keys(this.columnVisibility);
+        allColumns.forEach((column) => {
+            this.columnVisibility[column] = this.displayedColumns.includes(column);
+        });
+    }
+
+    applyColumnVisibility(): void {
+        const displayedColumnsTemp: string[] = [];
+
+        Object.keys(this.columnVisibility).forEach((column) => {
+            if (this.columnVisibility[column]) {
+                displayedColumnsTemp.push(column);
+            }
+        });
+
+        this.displayedColumns = displayedColumnsTemp;
+        this.dataSource = new MatTableDataSource<CalendarTag>(this.tags);
+
+        this.showColumnMenu = false;
+    }
+
+    onSearch(text: string) {
+        console.log('Search:', text);
+    }
+
     onClickAdd() {
         console.log('Add');
+    }
+
+    onClickView(tag: CalendarTag) {
+        console.log('View:', tag);
+        this.modalTagService.openDialog('view', tag);
+    }
+
+    onClickEdit(tag: CalendarTag) {
+        console.log('Edit:', tag);
+        this.modalTagService.openDialog('edit', tag);
+    }
+
+    onClickDelete(tag: CalendarTag) {
+        this.dataSource.data = this.dataSource.data.filter((item) => item !== tag);
     }
 }
--- src/app/features/components/tag/tag.module.ts
+++ src/app/features/components/tag/tag.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TagComponent } from './tag.component';
 import { RouterModule } from '@angular/router';
-import { TagListModule } from '../tag-list/tag-list.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { TableListModule } from '../table-list/table-list.module';
+import { MenagementTagComponent } from '../../modals/menagement-tag/menagement-tag.component';
 
 @NgModule({
-    declarations: [TagComponent],
+    declarations: [TagComponent, MenagementTagComponent],
     imports: [
         CommonModule,
-        TagListModule,
+        FontAwesomeModule,
+        MatTableModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatSelectModule,
+        MatMenuModule,
+        TableListModule,
         RouterModule.forChild([
             {
                 path: '',
