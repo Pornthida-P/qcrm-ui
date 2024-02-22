bf8e0d6f8f9993d6118d48aff6488f019df8898a
MrToNGx0 <tongsukkha@gmail.com>
2024-02-22 15:07:46 +0700

    add function get data user #80

--- src/app/features/components/home/home.component.ts
+++ src/app/features/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatCalendar, MatCalendarCellClassFunction } from '@angular/material/datepicker';
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import * as moment from 'moment';
@@ -6,24 +6,30 @@ import { tap, catchError } from 'rxjs';
 import { CalendarEventService } from 'src/app/services/calendar-event/calendar-event.service';
 import { ModalCalendarService } from 'src/app/services/modal-calendar/modal-calendar.service';
 import { SweetAlertService } from 'src/app/services/sweet-alert/sweet-alert.service';
+import { UserService } from 'src/app/services/user/user.service';
 import { CalendarEvent } from 'src/app/shared/interface/calendar.interface';
+import { User } from 'src/app/shared/interface/user.interface';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
     @ViewChild(MatCalendar, { static: false }) calendar!: MatCalendar<Date>;
+
     selectedCalendarDate: Date | null = new Date();
     calendarDateEvents: CalendarEvent[] = [];
     events: any = [];
+    userData: User | null = null;
+    isAction: boolean = false;
 
     faPlus = faPlusCircle;
 
     title: string = 'หน้าหลัก';
 
     constructor(
+        private userService: UserService,
         private modalCalendarService: ModalCalendarService,
         private calendarService: CalendarEventService,
         private sweetalertServices: SweetAlertService,
@@ -40,7 +46,6 @@ export class HomeComponent {
             .getAllCalendarEvent()
             .pipe(
                 tap((events) => {
-                    console.log('events', events);
                     this.calendarDateEvents = events;
                     this.onSelectedDateChanged();
                 }),
@@ -50,6 +55,13 @@ export class HomeComponent {
                 }),
             )
             .subscribe();
+    }
+
+    getUserData() {
+        this.userService.getDataUser().subscribe((res: User | null) => {
+            this.userData = res;
+            this.isAction = this.userData?.role.roleTitle.toLowerCase() === 'admin' ? true : false;
+        });
     }
 
     dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
@@ -94,7 +106,6 @@ export class HomeComponent {
     }
 
     onSelectDate(date: Date) {
-        console.log('date', date);
         this.selectedCalendarDate = date;
         this.onSelectedDateChanged();
     }
