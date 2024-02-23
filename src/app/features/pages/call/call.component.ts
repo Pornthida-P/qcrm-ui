1c5733655917b4bb5a9b63554b6d3faffec821ca
Benjapronben <121014441+Benjapronben@users.noreply.github.com>
2024-02-23 13:39:37 +0700

    จัด format

--- src/app/features/pages/call/call.component.ts
+++ src/app/features/pages/call/call.component.ts
@@ -62,214 +62,209 @@ export class SearchPipe implements PipeTransform {
 // }
 
 @Component({
-  selector: 'app-call',
-  templateUrl: './call.component.html',
-  styleUrls: ['./call.component.scss'],
+    selector: 'app-call',
+    templateUrl: './call.component.html',
+    styleUrls: ['./call.component.scss'],
 })
 export class CallComponent implements OnInit {
-  value: string | undefined;
+    value: string | undefined;
 
-  calls: any[] = [];
-  selectedCalls: any = [];
+    calls: any[] = [];
+    selectedCalls: any = [];
 
-  faPenToSquare = faPenToSquare;
-  faTrashCan = faTrashCan;
-  faArrowRight = faArrowRight;
-  faArrowLeft = faArrowLeft;
-  faCircleXmark = faCircleXmark;
+    faPenToSquare = faPenToSquare;
+    faTrashCan = faTrashCan;
+    faArrowRight = faArrowRight;
+    faArrowLeft = faArrowLeft;
+    faCircleXmark = faCircleXmark;
 
-  visibleRightSideBar: boolean = true;
-  visibleLeftSideBar: boolean = true;
-  detailItem: any = undefined;
-  emptyItem: String = 'ว่าง';
-  itemIdex: number = 0;
-  isAction: boolean = false;
+    visibleRightSideBar: boolean = true;
+    visibleLeftSideBar: boolean = true;
+    detailItem: any = undefined;
+    emptyItem: String = 'ว่าง';
+    itemIdex: number = 0;
+    isAction: boolean = false;
 
-  pageSizeOptions = [10, 20];
-  pageSize = 10;
-  currentPage = 1;
-  totalItems = 0;
-  totalPages = 0;
-  pagesToShow = 3;
+    pageSizeOptions = [10, 20];
+    pageSize = 10;
+    currentPage = 1;
+    totalItems = 0;
+    totalPages = 0;
+    pagesToShow = 3;
 
-  filterOption!: any[];
-  selectedFilter: any | undefined;
+    filterOption!: any[];
+    selectedFilter: any | undefined;
 
-  valueSearch!: string;
+    valueSearch!: string;
 
-  userData: any;
+    userData: any;
 
-  sortId: string = '-';
-  sortOrder: string = 'ASC';
-  sortIcon: string = '';
-  checkedValues: any;
-  selectValue: number[] = [];
+    sortId: string = '-';
+    sortOrder: string = 'ASC';
+    sortIcon: string = '';
+    checkedValues: any;
+    selectValue: number[] = [];
 
-  constructor(
-    private callService: CallService,
-    private router: Router,
-    private activeRoute: ActivatedRoute,
-    private userService: UserService,
-  ) { }
+    constructor(
+        private callService: CallService,
+        private router: Router,
+        private activeRoute: ActivatedRoute,
+        private userService: UserService,
+    ) {}
 
-  ngOnInit() {
-    this.getUserData();
+    ngOnInit() {
+        this.getUserData();
 
-    this.filterOption = [
-      { name: 'ทั้งหมด', code: 'all' },
-      { name: 'Only My', code: this.userData.username },
-    ];
+        this.filterOption = [
+            { name: 'ทั้งหมด', code: 'all' },
+            { name: 'Only My', code: this.userData.username },
+        ];
 
-    this.activeRoute.queryParams.subscribe((params) => {
-      if (params['cb'] != undefined && params['cb'] != '') {
-        const cbArray = params['cb'].split(',').map(Number);
-        this.pageSize = cbArray[0];
-        this.currentPage = cbArray[1];
-        this.totalItems = cbArray[2];
-        this.totalPages = cbArray[3];
-      }
-    });
-    this.selectedFilter = this.filterOption[0].code;
-    this.getCallsData((this.currentPage - 1) * this.pageSize, this.pageSize);
-    this.getPage();
-  }
-
-  async getCallsData(page: number, pageSize: number) {
-    await this.callService.getCallsPage(page, pageSize, `${this.sortId},${this.sortOrder}`).subscribe((res: any) => {
-      this.calls = Object.values(res);
-    });
-  }
-
-  async getCallsSide(page: number, pageSize: number, value: string) {
-    await this.callService.getCalls(page, pageSize).subscribe((res: any) => {
-      this.calls = Object.values(res);
-      if (value == 'right') this.showSideBar(0);
-      else if (value == 'left') this.showSideBar(this.pageSize - 1);
-    });
-  }
-
-  showSideBar(value: number) {
-    console.log('SideBar');
-    this.itemIdex = value;
-    this.detailItem = this.calls[this.itemIdex];
-    this.visibleLeftSideBar = true;
-    this.visibleRightSideBar = true;
-
-    if (this.itemIdex == 0 && this.currentPage == 1) this.visibleLeftSideBar = false;
-    if (this.itemIdex == this.calls.length - 1 && this.currentPage == this.totalPages) this.visibleRightSideBar = false;
-  }
-
-  // editPage(item: any) {
-  //     console.log('go to editpage');
-  //     console.log(item);
-  //     const cb = `${this.pageSize},${this.currentPage},${this.totalItems},${this.totalPages}`;
-  //     this.router.navigate(['/'], { queryParams: { itemId: item.activityTopicId, cb: cb } });
-  // }
-
-  editPage() {
-    this.router.navigate(['/call/edit']);
-  }
-
-  async pageChange(page: number) {
-    console.log(`${this.pageSize},${this.currentPage},${this.totalItems},${this.totalPages}`);
-    if (page != this.currentPage) {
-      if (page >= 1 && page <= this.totalPages) {
-        this.currentPage = page;
-        await this.getCallsData((this.currentPage - 1) * this.pageSize, this.pageSize);
-      }
+        this.activeRoute.queryParams.subscribe((params) => {
+            if (params['cb'] != undefined && params['cb'] != '') {
+                const cbArray = params['cb'].split(',').map(Number);
+                this.pageSize = cbArray[0];
+                this.currentPage = cbArray[1];
+                this.totalItems = cbArray[2];
+                this.totalPages = cbArray[3];
+            }
+        });
+        this.selectedFilter = this.filterOption[0].code;
+        this.getCallsData((this.currentPage - 1) * this.pageSize, this.pageSize);
+        this.getPage();
     }
-  }
 
-  get pages(): number[] {
-    var page: number[] = [];
-    this.totalPages = Math.ceil(this.totalItems / this.pageSize);
-    for (var i = -this.pagesToShow; i <= this.pagesToShow; i++) {
-      if (this.currentPage + i > 0 && this.currentPage + i <= this.totalPages) {
-        page.push(this.currentPage + i);
-      }
+    async getCallsData(page: number, pageSize: number) {
+        await this.callService.getCallsPage(page, pageSize, `${this.sortId},${this.sortOrder}`).subscribe((res: any) => {
+            this.calls = Object.values(res);
+        });
     }
-    return page;
-  }
 
-  pageSizeChange() {
-    this.currentPage = 1;
-    this.getCallsData((this.currentPage - 1) * this.pageSize, this.pageSize);
-  }
-
-  async changeSideBar(value: string) {
-    if (value == 'right') {
-      if (this.itemIdex >= this.pageSize - 1) {
-        await this.pageChangeSideBar(this.currentPage + 1, value);
-      } else {
-        this.showSideBar(this.itemIdex + 1);
-      }
-    } else if (value == 'left') {
-      if (this.itemIdex == 0) {
-        await this.pageChangeSideBar(this.currentPage - 1, value);
-      } else {
-        this.showSideBar(this.itemIdex - 1);
-      }
+    async getCallsSide(page: number, pageSize: number, value: string) {
+        await this.callService.getCalls(page, pageSize).subscribe((res: any) => {
+            this.calls = Object.values(res);
+            if (value == 'right') this.showSideBar(0);
+            else if (value == 'left') this.showSideBar(this.pageSize - 1);
+        });
     }
-  }
 
-  pageChangeSideBar(page: number, value: string): void {
-    if (page != this.currentPage) {
-      if (page >= 1 && page <= this.totalPages) {
-        this.currentPage = page;
-        this.getCallsSide((this.currentPage - 1) * this.pageSize, this.pageSize, value);
-      }
+    showSideBar(value: number) {
+        console.log('SideBar');
+        this.itemIdex = value;
+        this.detailItem = this.calls[this.itemIdex];
+        this.visibleLeftSideBar = true;
+        this.visibleRightSideBar = true;
+
+        if (this.itemIdex == 0 && this.currentPage == 1) this.visibleLeftSideBar = false;
+        if (this.itemIdex == this.calls.length - 1 && this.currentPage == this.totalPages) this.visibleRightSideBar = false;
     }
-  }
 
-  getPage() {
-    this.callService.getCallsCount().subscribe((res: any) => {
-      this.totalItems = res.count;
-    });
-  }
+    // editPage(item: any) {
+    //     console.log('go to editpage');
+    //     console.log(item);
+    //     const cb = `${this.pageSize},${this.currentPage},${this.totalItems},${this.totalPages}`;
+    //     this.router.navigate(['/'], { queryParams: { itemId: item.activityTopicId, cb: cb } });
+    // }
 
-  search() {
-    this.calls.filter((item: any) => item.mobilePhone.toLowerCase().includes(this.valueSearch.toLowerCase()));
-  }
+    editPage() {
+        this.router.navigate(['/call/edit']);
+    }
 
-  createCall() {
-    this.router.navigate(['/call/create']);
-  }
-
-  getUserData() {
-    this.userService.getDataUser().subscribe((res: User | null) => {
-      this.userData = res;
-      this.isAction = res?.role.roleTitle.toLocaleLowerCase() === 'admin' ? true : false;
-    });
-  }
-
-  deleteCall() {
-    Swal.fire({
-      icon: 'warning',
-      title: 'Do you want to delete this form?',
-      showCancelButton: true,
-      confirmButtonColor: '#3066be',
-      cancelButtonColor: '#ec5365',
-      width: '50%',
-    });
-  }
-
-  exportExcel() {
-
-  }
-
-
-    deleteSelectForm() {
-  }
-
-  selectCheckbox(event: any, callId: number): void {
-    if (event.target.checked) {
-        if (!this.selectValue.includes(callId)) {
-            this.selectValue.push(callId);
+    async pageChange(page: number) {
+        console.log(`${this.pageSize},${this.currentPage},${this.totalItems},${this.totalPages}`);
+        if (page != this.currentPage) {
+            if (page >= 1 && page <= this.totalPages) {
+                this.currentPage = page;
+                await this.getCallsData((this.currentPage - 1) * this.pageSize, this.pageSize);
+            }
         }
-    } else {
-        this.selectValue = this.selectValue.filter((id: number) => id !== callId);
     }
-    // console.log('this.selectValue', this.selectValue);
-}
 
-  }
+    get pages(): number[] {
+        var page: number[] = [];
+        this.totalPages = Math.ceil(this.totalItems / this.pageSize);
+        for (var i = -this.pagesToShow; i <= this.pagesToShow; i++) {
+            if (this.currentPage + i > 0 && this.currentPage + i <= this.totalPages) {
+                page.push(this.currentPage + i);
+            }
+        }
+        return page;
+    }
+
+    pageSizeChange() {
+        this.currentPage = 1;
+        this.getCallsData((this.currentPage - 1) * this.pageSize, this.pageSize);
+    }
+
+    async changeSideBar(value: string) {
+        if (value == 'right') {
+            if (this.itemIdex >= this.pageSize - 1) {
+                await this.pageChangeSideBar(this.currentPage + 1, value);
+            } else {
+                this.showSideBar(this.itemIdex + 1);
+            }
+        } else if (value == 'left') {
+            if (this.itemIdex == 0) {
+                await this.pageChangeSideBar(this.currentPage - 1, value);
+            } else {
+                this.showSideBar(this.itemIdex - 1);
+            }
+        }
+    }
+
+    pageChangeSideBar(page: number, value: string): void {
+        if (page != this.currentPage) {
+            if (page >= 1 && page <= this.totalPages) {
+                this.currentPage = page;
+                this.getCallsSide((this.currentPage - 1) * this.pageSize, this.pageSize, value);
+            }
+        }
+    }
+
+    getPage() {
+        this.callService.getCallsCount().subscribe((res: any) => {
+            this.totalItems = res.count;
+        });
+    }
+
+    search() {
+        this.calls.filter((item: any) => item.mobilePhone.toLowerCase().includes(this.valueSearch.toLowerCase()));
+    }
+
+    createCall() {
+        this.router.navigate(['/call/create']);
+    }
+
+    getUserData() {
+        this.userService.getDataUser().subscribe((res: User | null) => {
+            this.userData = res;
+            this.isAction = res?.role.roleTitle.toLocaleLowerCase() === 'admin' ? true : false;
+        });
+    }
+
+    deleteCall() {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Do you want to delete this form?',
+            showCancelButton: true,
+            confirmButtonColor: '#3066be',
+            cancelButtonColor: '#ec5365',
+            width: '50%',
+        });
+    }
+
+    exportExcel() {}
+
+    deleteSelectForm() {}
+
+    selectCheckbox(event: any, callId: number): void {
+        if (event.target.checked) {
+            if (!this.selectValue.includes(callId)) {
+                this.selectValue.push(callId);
+            }
+        } else {
+            this.selectValue = this.selectValue.filter((id: number) => id !== callId);
+        }
+        // console.log('this.selectValue', this.selectValue);
+    }
+}
