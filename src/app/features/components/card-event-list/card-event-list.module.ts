4fc126350572d0c7e161c28081bad422802e4093
MrToNGx0 <tongsukkha@gmail.com>
2024-02-27 15:05:23 +0700

    เพิ่ม toolbar filter #80

--- src/app/features/components/card-event-list/card-event-list.module.ts
+++ src/app/features/components/card-event-list/card-event-list.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { CardEventListComponent } from './card-event-list.component';
 import { CalendarCardModule } from '../calendar-card/calendar-card.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [CardEventListComponent],
-    imports: [CommonModule, CalendarCardModule, FontAwesomeModule],
+    imports: [CommonModule, CalendarCardModule, FontAwesomeModule, ReactiveFormsModule, FormsModule],
     exports: [CardEventListComponent],
 })
 export class CardEventListModule {}
