60478fecc09bc40e10e59fdafa1b627044109408
Benjapronben <121014441+Benjapronben@users.noreply.github.com>
2024-03-04 14:11:02 +0700

    add function format date and time #54

--- src/app/features/pages/create-call/create-call.component.ts
+++ src/app/features/pages/create-call/create-call.component.ts
@@ -6,6 +6,7 @@ import { CallService } from 'src/app/services/call/call.service';
 import { ContactService } from 'src/app/services/contact/contact.service';
 import { catchError, tap } from 'rxjs';
 import { SweetAlertService } from 'src/app/services/sweet-alert/sweet-alert.service';
+
 @Component({
     selector: 'app-create-call',
     templateUrl: './create-call.component.html',
@@ -27,10 +28,14 @@ export class CreateCallComponent {
     direction: string = '';
     duration: string = '';
     description: string = '';
+    timepickStart: any;
+    hour: any;
 
     selectedItem: any;
     selectedData: any[] = [];
     detailItem: any;
+    combinedDateTimeStart: string = '';
+    combinedDateTimeEnd: string = '';
 
     constructor(
         private location: Location,
@@ -38,14 +43,13 @@ export class CreateCallComponent {
         private callServive: CallService,
         private sweetalertServices: SweetAlertService,
     ) {
-      this.selectedDate = new Date();
+        this.selectedDate = new Date();
     }
 
     prev() {
         this.location.back();
     }
 
-    timepickStart = true;
     timepickEnd = true;
     meridian = true;
     seconds = true;
@@ -54,10 +58,59 @@ export class CreateCallComponent {
     date = new FormControl(new Date());
     serializedDate = new FormControl(new Date().toISOString());
 
+    formatStartDate() {
+        const startDate = new Date(this.startTime);
+
+        const formattedStartDate = startDate.toISOString().split('T')[0];
+
+        this.startTime = formattedStartDate;
+    }
+
+    formatEndDate() {
+        const endDate = new Date(this.endTime);
+
+        const formattedEndDate = endDate.toISOString().split('T')[0];
+
+        this.endTime = formattedEndDate;
+    }
+
+    formatTimepickStart() {
+        const startTimepick = new Date(this.timepickStart);
+        const formatTimepickStart = startTimepick.toISOString();
+        this.timepickStart = formatTimepickStart;
+    }
+
+    onTimepickStartChange(event: any) {
+        const hour = event.hour;
+        const minute = event.minute;
+        const second = event.second;
+
+        this.timepickStart = event;
+
+        const formattedTimeStartPick = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${second
+            .toString()
+            .padStart(2, '0')}`;
+
+        this.combinedDateTimeStart = `${this.startTime} ${formattedTimeStartPick}`;
+    }
+
+    onTimepickEndChange(event: any) {
+        const hour = event.hour;
+        const minute = event.minute;
+        const second = event.second;
+
+        this.timepickEnd = event;
+
+        const formattedTimeEndPick = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${second
+            .toString()
+            .padStart(2, '0')}`;
+
+        this.combinedDateTimeEnd = `${this.endTime} ${formattedTimeEndPick}`;
+    }
+
     ngOnInit(): void {
         this.route.queryParams.subscribe((params: any) => {
             this.contactId = params['contactId'];
-            console.log('contactId', this.contactId);
         });
     }
 
@@ -84,32 +137,31 @@ export class CreateCallComponent {
         }
     }
 
-  submit() {
-      console.log(this.parentSub)
-      const userData = JSON.parse(localStorage.getItem('userData') || '{}');
-      const data = {
-          contactId: this.contactId,
-          name: this.contactName,
-          status: this.status,
-          direction: this.direction,
-          caseTopicName: this.parentSub,
-          description: this.description,
-          startTime: this.startTime,
-          endTime: this.endTime,
-          duration: this.duration,
-          createdById: userData.userId,
-      };
-      this.callServive
-          .createCalls(data)
-          .pipe(
-              tap((res) => {
-                  this.sweetalertServices.getSwal('success', 'Save data success.', '', false, '/contacts');
-              }),
-              catchError((error) => {
-                  this.sweetalertServices.handleError(error);
-                  throw error;
-              }),
-          )
-          .subscribe();
-  }
+    submit() {
+        const userData = JSON.parse(localStorage.getItem('userData') || '{}');
+        const data = {
+            contactId: this.contactId,
+            name: this.contactName,
+            status: this.status,
+            direction: this.direction,
+            caseTopicName: this.parentSub,
+            description: this.description,
+            startTime: this.combinedDateTimeStart,
+            endTime: this.combinedDateTimeEnd,
+            duration: this.duration,
+            createdById: userData.userId,
+        };
+        this.callServive
+            .createCalls(data)
+            .pipe(
+                tap((res) => {
+                    this.sweetalertServices.getSwal('บันทึกข้อมูลเรียบร้อยแล้ว', 'Save data success.', '', false, '/contacts');
+                }),
+                catchError((error) => {
+                    this.sweetalertServices.handleError(error);
+                    throw error;
+                }),
+            )
+            .subscribe();
+    }
 }
