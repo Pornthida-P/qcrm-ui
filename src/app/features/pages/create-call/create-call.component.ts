e0764f8624e3fff2d8865d3e8e049a42155e50b3
Benjapronben <121014441+Benjapronben@users.noreply.github.com>
2024-03-06 17:35:01 +0700

    change layout and add NgbTypeaheadModule #54

--- src/app/features/pages/create-call/create-call.component.ts
+++ src/app/features/pages/create-call/create-call.component.ts
@@ -3,8 +3,7 @@ import { Location } from '@angular/common';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { CallService } from 'src/app/services/call/call.service';
-import { ContactService } from 'src/app/services/contact/contact.service';
-import { catchError, tap } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, map, Observable, OperatorFunction, tap } from 'rxjs';
 import { SweetAlertService } from 'src/app/services/sweet-alert/sweet-alert.service';
 
 @Component({
@@ -21,7 +20,6 @@ export class CreateCallComponent {
 
     contactName!: string;
     status: string = '';
-    parent: string = '';
     parentSub: any;
     startTime: string = '';
     endTime: string = '';
@@ -37,6 +35,11 @@ export class CreateCallComponent {
     detailItem: any;
     combinedDateTimeStart: string = '';
     combinedDateTimeEnd: string = '';
+
+    parent: any = null;
+
+    model: any;
+    organizationName: string | undefined;
 
     constructor(
         private location: Location,
@@ -113,30 +116,43 @@ export class CreateCallComponent {
         this.route.queryParams.subscribe((params: any) => {
             this.contactId = params['contactId'];
         });
-    }
 
-    loadData(option: string) {
-        switch (option) {
-            case 'contacts':
-                this.callServive.getAllContacts().subscribe((contacts: any) => {
-                    this.selectedData = contacts;
-                });
-                break;
-            case 'organizations':
-                this.callServive.getOrganizations().subscribe((organizations: any) => {
-                    this.selectedData = organizations;
-                });
-                break;
-            case 'casetopics':
-                this.callServive.getCaseTopic().subscribe((casetopics: any) => {
-                    this.selectedData = casetopics;
-                });
-                break;
-            default:
-                this.selectedData = [];
-                break;
-        }
+        this.callServive.getCaseTopic().subscribe((casetopics: any) => {
+            this.casetopic = casetopics;
+        });
+
+        this.callServive.getOrganizations().subscribe((organizations: any) => {
+          this.organizations = organizations;
+      });
+
+
     }
+  searchFunction() {
+    throw new Error('Method not implemented.');
+  }
+
+    // loadData(option: string) {
+    //     switch (option) {
+    //         case 'contacts':
+    //             this.callServive.getAllContacts().subscribe((contacts: any) => {
+    //                 this.selectedData = contacts;
+    //             });
+    //             break;
+    //         case 'organizations':
+    //             this.callServive.getOrganizations().subscribe((organizations: any) => {
+    //                 this.selectedData = organizations;
+    //             });
+    //             break;
+    //         case 'casetopics':
+    //             this.callServive.getCaseTopic().subscribe((casetopics: any) => {
+    //                 this.selectedData = casetopics;
+    //             });
+    //             break;
+    //         default:
+    //             this.selectedData = [];
+    //             break;
+    //     }
+    // }
 
     submit() {
         const userData = JSON.parse(localStorage.getItem('userData') || '{}');
@@ -157,7 +173,7 @@ export class CreateCallComponent {
             .createCalls(data)
             .pipe(
                 tap((res) => {
-                    this.sweetalertServices.getSwal('success','บันทึกข้อมูลเรียบร้อยแล้ว', '', false, '/contacts');
+                    this.sweetalertServices.getSwal('success', 'บันทึกข้อมูลเรียบร้อยแล้ว', '', false, '/contacts');
                 }),
                 catchError((error) => {
                     this.sweetalertServices.handleError(error);
@@ -166,4 +182,5 @@ export class CreateCallComponent {
             )
             .subscribe();
     }
+
 }
