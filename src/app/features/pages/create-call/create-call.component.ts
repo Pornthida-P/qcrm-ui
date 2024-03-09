762a04e09145099703e7cc462ce95656e5718d21
Benjapronben <121014441+Benjapronben@users.noreply.github.com>
2024-03-10 00:32:21 +0700

    change form create call #54

--- src/app/features/pages/create-call/create-call.component.ts
+++ src/app/features/pages/create-call/create-call.component.ts
@@ -5,7 +5,8 @@ import { ActivatedRoute } from '@angular/router';
 import { CallService } from 'src/app/services/call/call.service';
 import { catchError, debounceTime, distinctUntilChanged, map, Observable, OperatorFunction, tap } from 'rxjs';
 import { SweetAlertService } from 'src/app/services/sweet-alert/sweet-alert.service';
-
+import { ContactsService } from 'src/app/services/contacts/contacts.service';
+import { faArrowLeft, faArrowRight, faPenToSquare, faTrashCan, faCircleXmark, faEye, faClipboard } from '@fortawesome/free-solid-svg-icons';
 @Component({
     selector: 'app-create-call',
     templateUrl: './create-call.component.html',
@@ -14,11 +15,11 @@ import { SweetAlertService } from 'src/app/services/sweet-alert/sweet-alert.serv
 export class CreateCallComponent {
     selectedDate: Date;
     contactId: string = '';
-    casetopic: any[] = [];
+    casetopics: any[] = [];
     organizations: any;
     contacts: any[] = [];
     casesubjects: any[] = [];
-    contactName!: string;
+    contactName: string = '';
     status: string = '';
     parentSub: any;
     startTime: string = '';
@@ -29,15 +30,18 @@ export class CreateCallComponent {
     timepickStart: any;
     hour: any;
     solutions: string = '';
+    contactOrgName: string = '';
 
     selectedItem: any;
     selectedData: any[] = [];
     detailItem: any;
     combinedDateTimeStart: string = '';
     combinedDateTimeEnd: string = '';
-    contactOrg: string = '';
-    selectedTopics: string[] = [];
+    contactOrg: any;
+    selectedTopics: any;
     selectedCasesubject: any;
+    selectedCaseTopics: any;
+    selectedChannels: any;
 
     parent: any = null;
 
@@ -47,11 +51,58 @@ export class CreateCallComponent {
     myForm: FormGroup | any; //
     channels: any;
 
+    visibleRightSideBar: boolean = true;
+    visibleLeftSideBar: boolean = true;
+    FormShowing: boolean = false;
+    SearchFormShowing: boolean = true;
+    SearchOrgShowing: boolean = false;
+    AddOrgShowing: boolean = false;
+    thanks: boolean = false;
+
+    pageSizeOptionOrgs = [5, 10, 20];
+    currentPage = 1;
+    currentPageOrg = 1;
+    totalItemOrgs = 0;
+    totalPageOrgs = 0;
+    pagesToShowOrg = 3;
+
+    pageSizeOptions = [5, 10, 20];
+    pageSizeOrg = 5;
+    pageSize = 5;
+    totalItems = 0;
+    totalPages = 0;
+    pagesToShow = 3;
+
+    sortIdOrg: string = 'createdAt';
+    sortOrderOrg: string = 'DESC';
+
+    valueSearch!: string;
+
+    valueSearchOrg!: string;
+    selectedFilter: any | undefined;
+    spareorganizations!: any;
+
+    sortIcon: string = '';
+
+    contactProductType: string = '';
+    checkedValueOrgs: string[] = [];
+
+    faCircleXmark = faCircleXmark;
+    sparecontacts: any;
+
+    userData: any = JSON.parse(localStorage.getItem('userData') || '{}');
+    userId: any;
+    activitiestype: any;
+    isEmailSubscribed: number = 0;
+    contactIdSelect: string = '';
+  activityTypeId: any;
+
     constructor(
         private location: Location,
         private route: ActivatedRoute,
         private callServive: CallService,
         private sweetalertServices: SweetAlertService,
+        private contactService: ContactsService,
     ) {
         this.selectedDate = new Date();
     }
@@ -124,15 +175,15 @@ export class CreateCallComponent {
         });
 
         this.callServive.getCaseTopic().subscribe((casetopics: any) => {
-            this.casetopic = casetopics;
+            this.casetopics = casetopics;
         });
 
         this.callServive.getOrganizations().subscribe((organizations: any) => {
             this.organizations = organizations;
         });
 
-        this.callServive.getAllContacts().subscribe((contacts: any) => {
-            this.contacts = contacts;
+        this.callServive.getActivitiesType().subscribe((activitiestype: any) => {
+            this.activitiestype = activitiestype;
         });
 
         this.callServive.getAllCaseSubjects().subscribe((casesubjects: any) => {
@@ -146,23 +197,31 @@ export class CreateCallComponent {
         this.myForm = new FormGroup({
             contactId: new FormControl(''),
         });
-    }
-    searchFunction() {
-        throw new Error('Method not implemented.');
+
+        this.selectedFilter = 'all';
+
+        if (this.selectedFilter !== 'all') {
+            this.userId = this.userData.userId;
+            console.log('user: ', this.userData.userId);
+        }
+
+        this.getFormContact((this.currentPageOrg - 1) * this.pageSizeOrg, this.pageSizeOrg);
+        this.getPageOrg();
     }
 
     submit() {
         const userData = JSON.parse(localStorage.getItem('userData') || '{}');
         const data = {
             contactId: this.contactId,
-            name: this.contactName,
-            status: this.status,
-            direction: this.direction,
-            caseTopicName: this.parentSub,
+            name: this.contactIdSelect,
+            organization: this.contactOrg,
+            caseTopicId: this.selectedCaseTopics,
+            caseSubject: this.selectedCasesubject,
+            channel: this.selectedChannels,
+            emailInfo: this.isEmailSubscribed,
+            activityType: this.activityTypeId,
             description: this.description,
             startTime: this.combinedDateTimeStart,
-            endTime: this.combinedDateTimeEnd,
-            duration: this.duration,
             solution: this.solutions,
             createdById: userData.userId,
         };
@@ -178,5 +237,99 @@ export class CreateCallComponent {
                 }),
             )
             .subscribe();
+    }
+    showSideBarOrg() {
+        this.visibleLeftSideBar = true;
+        this.visibleRightSideBar = true;
+        this.FormShowing = false;
+        this.SearchFormShowing = true;
+        this.SearchOrgShowing = true;
+        this.thanks = false;
+        this.AddOrgShowing = false;
+    }
+
+    pageSizeChangeOrg() {
+        this.currentPage = 1;
+        this.getFormContact((this.currentPageOrg - 1) * this.pageSizeOrg, this.pageSizeOrg);
+    }
+
+    async getFormContact(pageOrg: number, pageSizeOrg: number) {
+        await this.callServive
+            .getContactByPage(pageOrg, pageSizeOrg, `${this.sortIdOrg},${this.sortOrderOrg}`, this.valueSearchOrg, this.selectedFilter)
+            .subscribe((res: any) => {
+                this.contacts = res;
+                this.sparecontacts = res;
+                console.log('contact:', this.contacts);
+            });
+    }
+
+    sortOrg(value: string) {
+        if (this.sortIdOrg == value) {
+            if (this.sortIcon == 'fa-solid fa-sort-down') {
+                this.sortIcon = 'fa-solid fa-sort-up';
+                this.sortOrderOrg = 'DESC';
+            } else {
+                this.sortIcon = 'fa-solid fa-sort-down';
+                this.sortOrderOrg = 'ASC';
+            }
+        } else {
+            this.sortIdOrg = value;
+        }
+        this.getFormContact((this.currentPageOrg - 1) * this.pageSizeOrg, this.pageSizeOrg);
+    }
+
+    chooseContact(contactsId: string) {
+        this.contactIdSelect = contactsId;
+        this.callServive.getContactById(contactsId).subscribe((res: any) => {
+            this.contactName = `${res[0].firstName} ${res[0].lastName}`;
+        });
+    }
+
+    async pageChangeOrg(pageOrg: number) {
+        if (pageOrg != this.currentPageOrg) {
+            if (pageOrg >= 1 && pageOrg <= this.totalPageOrgs) {
+                this.currentPageOrg = pageOrg;
+                await this.getFormContact((this.currentPageOrg - 1) * this.pageSizeOrg, this.pageSizeOrg);
+                this.checkedValueOrgs = [];
+            }
+        }
+    }
+
+    get pageOrgs(): number[] {
+        var pageOrg: number[] = [];
+        this.totalPageOrgs = Math.ceil(this.totalItemOrgs / this.pageSizeOrg);
+        for (var i = -this.pagesToShowOrg; i <= this.pagesToShowOrg; i++) {
+            if (this.currentPageOrg + i > 0 && this.currentPageOrg + i <= this.totalPageOrgs) {
+                pageOrg.push(this.currentPageOrg + i);
+            }
+        }
+        return pageOrg;
+    }
+
+    searchOrg() {
+        if (this.selectedFilter !== 'all') {
+            this.userId = this.userData.userId;
+        } else {
+            this.userId = '';
+        }
+        this.getFormContact((this.currentPageOrg - 1) * this.pageSizeOrg, this.pageSizeOrg);
+        this.getPageOrg();
+    }
+
+    async getPageOrg() {
+        await this.callServive.countContact(this.valueSearchOrg, this.userId).subscribe((res: any) => {
+            this.totalItemOrgs = res.count;
+        });
+    }
+
+    onCheckboxChange(event: any, activityTypeId: number) {
+        if (event.target.checked) {
+            console.log('Selected activity type ID:', activityTypeId);
+        }
+    }
+
+    toggleEmailSubscription(event: any) {
+        this.isEmailSubscribed = event.target.checked ? 1 : 0;
+        console.log('email:', this.isEmailSubscribed);
     }
 }
