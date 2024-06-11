b9b7bc37855e1c2da4db81c469f5580fe3827153
Benjapronben <121014441+Benjapronben@users.noreply.github.com>
2024-06-11 17:35:49 +0700

    change service get call #223

--- src/app/services/call/call.service.ts
+++ src/app/services/call/call.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { end } from '@popperjs/core';
 import { Observable, of } from 'rxjs';
 import { config } from 'src/app/config/config';
 import { environment } from 'src/environments/environment';
@@ -15,21 +16,51 @@ export class CallService {
         return this.http.get(`${this.baseUrl}${config.api.path.call.url}/${page}/${limit}`);
     }
 
-    getCallsPage(page: number, limit: number, sortId: string, searchText: string, createdBy: string) {
+    getCallsPage(
+        page: number,
+        limit: number,
+        sortId: string,
+        searchText: string,
+        createdBy: string,
+        dateFilterType: string,
+        startDate: string,
+        endDate: string,
+    ) {
         if (searchText == '' || searchText == null) {
             searchText = 'undefined';
         }
-        return this.http.get(`${this.baseUrl}${config.api.path.call.url}/${page}/${limit}/${sortId}/${searchText}/${createdBy}`);
+        if (dateFilterType == '' || dateFilterType == null) {
+            dateFilterType = 'undefined';
+        }
+        if (startDate == '' || startDate == null) {
+            startDate = 'undefined';
+        }
+        if (endDate == '' || endDate == null) {
+            endDate = 'undefined';
+        }
+        return this.http.get(
+            `${this.baseUrl}${config.api.path.call.url}/${page}/${limit}/${sortId}/${searchText}/${createdBy}/${dateFilterType}/${startDate}/${endDate}`,
+        );
     }
 
-    getCallsCount(searchText: string, createdById: string) {
+    getCallsCount(searchText: string, createdById: string, dateFilterType: string, startDate: string,
+      endDate: string,) {
         if (searchText == '' || searchText == null) {
             searchText = 'undefined';
         }
         if (createdById == '' || createdById == null) {
             createdById = 'undefined';
         }
-        return this.http.get(`${this.baseUrl}${config.api.path.call.url}${config.api.path.call.count}/${searchText}/${createdById}`);
+        if (dateFilterType == '' || dateFilterType == null) {
+            dateFilterType = 'undefined';
+        }
+        if (startDate == '' || startDate == null) {
+            startDate = 'undefined';
+        }
+        if (endDate == '' || endDate == null) {
+            endDate = 'undefined';
+        }
+        return this.http.get(`${this.baseUrl}${config.api.path.call.url}${config.api.path.call.count}/${searchText}/${createdById}/${dateFilterType}/${startDate}/${endDate}`);
     }
 
     getCaseTopic() {
@@ -70,11 +101,11 @@ export class CallService {
     }
 
     getActivitiesTypeSmn(searchText: string) {
-      if (searchText == '' || searchText == null) {
-          searchText = 'undefined';
-      }
-      return this.http.get(`${this.baseUrl}${config.api.path.call.url}${config.api.path.call.activitySmn}/${searchText}`);
-  }
+        if (searchText == '' || searchText == null) {
+            searchText = 'undefined';
+        }
+        return this.http.get(`${this.baseUrl}${config.api.path.call.url}${config.api.path.call.activitySmn}/${searchText}`);
+    }
 
     createCalls(data: any) {
         return this.http.post(`${this.baseUrl}${config.api.path.call.url}`, data);
@@ -114,16 +145,14 @@ export class CallService {
         if (searchText == '' || searchText == null) {
             searchText = 'undefined';
         }
-        return this.http.get(
-            `${this.baseUrl}${config.api.path.call.url}${config.api.path.call.activitybyidBypage}/${searchText}`,
-        );
+        return this.http.get(`${this.baseUrl}${config.api.path.call.url}${config.api.path.call.activitybyidBypage}/${searchText}`);
     }
 
     getCallById(id: string) {
-      return this.http.get(`${this.baseUrl}${config.api.path.call.url}${config.api.path.call.callById}/${id}`);
-  }
+        return this.http.get(`${this.baseUrl}${config.api.path.call.url}${config.api.path.call.callById}/${id}`);
+    }
 
     getCaseById(id: string) {
-      return this.http.get(`${this.baseUrl}${config.api.path.call.url}${config.api.path.call.caseById}/${id}`);
-  }
+        return this.http.get(`${this.baseUrl}${config.api.path.call.url}${config.api.path.call.caseById}/${id}`);
+    }
 }
