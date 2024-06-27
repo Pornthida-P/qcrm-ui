2f8cad71465782f98766a7e964cc0277d90f6cae
Benjapronben <121014441+Benjapronben@users.noreply.github.com>
2024-06-27 13:21:26 +0700

    add path get contact-email #314

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -62,6 +62,7 @@ export const config = {
                 surveyForm: '/survey-form',
                 call: '/call',
                 contactNumberFide: '/contact-number',
+                findEmailByContact: '/contact-email',
             },
             call: {
                 url: '/call',
